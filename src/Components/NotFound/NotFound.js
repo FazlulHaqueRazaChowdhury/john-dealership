806eae706cf2e1ce99f7286d158840527bb166b9
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-02 20:13:15 +0600

    added not found page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,21 @@
+import { MDBBtn } from 'mdb-react-ui-kit';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            404 error
+        <div className='m-100 d-flex justify-content-center align-items-center' style={{
+            backgroundImage: `url('https://webdeasy.de/wp-content/uploads/2020/06/404-pages.jpg')`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            minHeight: '100vh',
+
+        }}>
+            <MDBBtn className='text-center' color='danger' onClick={() => {
+                navigate('/')
+            }}>Go Back Home</MDBBtn>
         </div>
     );
 };
